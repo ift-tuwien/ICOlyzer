912e713c6cb9058436efe9d0e9346b157ce51941
René Schwaiger <sanssecours@me.com>
2026-04-01 17:37:37 +0200

    ICOcutter: Add command line parsing

--- icolyzer/cli.py
+++ icolyzer/cli.py
@@ -5,7 +5,6 @@
 from argparse import ArgumentTypeError
 from pathlib import Path
 
-
 # -- Functions ----------------------------------------------------------------
 
 
@@ -38,3 +37,61 @@ def file_exists(filepath: str) -> str:
         raise ArgumentTypeError(f"“{filepath}” does not point to a file")
 
     return filepath
+
+
+def measurement_time(value: str) -> float:
+    """Check if the given number is valid measurement time
+
+    Returns:
+
+        A float value representing the measurement time on success
+
+    Raises:
+
+        ArgumentTypeError:
+
+             If the given text is not a valid measurement time value
+
+    Examples:
+
+        Parse correct measurement times
+
+
+        >>> measurement_time("0.1")
+        0.1
+
+        >>> measurement_time("12.34")
+        12.34
+
+        0 is not a valid measurement time
+
+        >>> measurement_time("0")
+        Traceback (most recent call last):
+           ...
+        argparse.ArgumentTypeError: “0” is not a valid measurement time
+
+        A measurement time has to be positive
+
+        >>> measurement_time("-1")
+        Traceback (most recent call last):
+           ...
+        argparse.ArgumentTypeError: “-1” is not a valid measurement time
+
+        A measurement time has to be a number
+
+        >>> measurement_time("something")
+        Traceback (most recent call last):
+           ...
+        argparse.ArgumentTypeError: “something” is not a valid measurement time
+
+    """
+
+    try:
+        number = float(value)
+        if number <= 0:
+            raise ValueError()
+        return number
+    except ValueError as error:
+        raise ArgumentTypeError(
+            f"“{value}” is not a valid measurement time"
+        ) from error
---
+++ icolyzer/cutter.py
@@ -0,0 +1,50 @@
+"""Split HDF5 measurement files into parts"""
+
+# -- Imports ------------------------------------------------------------------
+
+from argparse import ArgumentParser, Namespace
+
+from icolyzer.cli import file_exists, measurement_time
+
+# -- Functions ----------------------------------------------------------------
+
+
+def get_arguments() -> Namespace:
+    """Parse command line arguments
+
+    Returns:
+
+        An object that contains the given command line arguments
+
+    """
+
+    parser = ArgumentParser(description="Split a HDF5 file into two parts")
+    parser.add_argument(
+        "input",
+        type=file_exists,
+        help="measurement data in HDF5 format",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--time",
+        type=measurement_time,
+        help=(
+            "the maximum timestamp in seconds that should still be part of "
+            "the first HDF5 file"
+        ),
+    )
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    """Split a HDF5 measurement file into two parts"""
+
+    get_arguments()
+
+
+# -- Main ---------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
