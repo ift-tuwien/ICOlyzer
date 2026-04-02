4c1c2fdd61dca3a3fd899ba1f48d4ca5336c9389
René Schwaiger <sanssecours@me.com>
2026-04-02 15:06:00 +0200

    ICOcutter: Create first part of split data

--- icolyzer/cutter.py
+++ icolyzer/cutter.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from sys import exit as sys_exit, stderr
 
 from pandas import read_hdf
+from tables import open_file
 
 from icolyzer.cli import file_exists, measurement_time
 
@@ -69,6 +70,29 @@ def main() -> None:
             f"Time to cut of “{cut_timedelta}” is equal or larger than "
             f"measurement time of “{measurement_timedelta}”",
         )
+
+    first_part_filepath = filepath.with_stem(f"{filepath.stem}-part-1")
+
+    with open_file(filepath, mode="r") as original:
+
+        # pylint: disable=fixme
+        # TODO: Remove overwrite=True
+        # pylint: enable=fixme
+        original.copy_file(first_part_filepath, overwrite=True)
+
+    with open_file(first_part_filepath, mode="r+") as copy:
+        data = copy.get_node("/acceleration")
+        cut_microseconds = cut_timedelta.total_seconds() * 1_000_000
+        last_row_to_include = 0
+        # pylint: disable=fixme
+        # TODO: Use faster algorithm (maybe binary search or something similar)
+        # pylint: enable=fixme
+        for row in data.iterrows():
+            if row["timestamp"] > cut_microseconds:
+                break
+            last_row_to_include += 1
+
+        data.remove_rows(last_row_to_include)
 
 
 # -- Main ---------------------------------------------------------------------
